162cb49be21a0b72f22818b94c0354f9a23cba34
Bogdan Melnik <113630039+WiZe1007@users.noreply.github.com>
2024-11-28 20:14:57 +0200

    Update DataAccess.cs

--- Lab_14/Task_8/DataAccess.cs
+++ Lab_14/Task_8/DataAccess.cs
@@ -5,7 +5,6 @@ namespace Lab__12
 {
     public static class DataAccess
     {
-        // Используйте ваш абсолютный путь к базе данных
         private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nemof\source\repos\Lab__12\Lab__12\SocialNetworkDB.mdf;Integrated Security=True;Connect Timeout=30;";
 
         public static SqlConnection GetConnection()
